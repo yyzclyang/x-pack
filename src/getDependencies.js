60d280d8646296bbf0ef944001c428a72570aeac
yyzcl <yyzclyang@outlook.com>
2020-11-26 02:13:09 +0800

    refactor: 依赖收集 requireContent 和 require文件的相对路径信息

--- src/getDependencies.js
+++ src/getDependencies.js
@@ -1,11 +1,23 @@
-function getDependencies(content) {
+const path = require('path');
+
+function getDependencies(filePath, fileContent) {
   const reg = /require\(['"](.+?)['"]/g;
   let result = null;
-  const mapping = [];
-  while ((result = reg.exec(content))) {
-    mapping.push(result[1]);
+  const dependencies = [];
+  while ((result = reg.exec(fileContent))) {
+    const requireContent = result[1];
+    const dependencyFilePath = path.resolve(
+      path.dirname(filePath),
+      requireContent
+    );
+    const dependencyFileRelativePath = path.relative(
+      process.cwd(),
+      dependencyFilePath
+    );
+
+    dependencies.push([requireContent, dependencyFileRelativePath]);
   }
-  return mapping;
+  return dependencies;
 }
 
 module.exports = { getDependencies };
